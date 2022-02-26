22e7ff0f1cb563cc94f4d325976c7edc0280f3bb
Andrey Dron <quankyyam@gmail.com>
2022-02-26 14:25:34 +0300

    add folder

---
+++ dron_a_a/build/prj.lab/rational/CMakeFiles/test_rational.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anreydron/CLionProjects/520_B/algos/dron_a_a/prj.lab/rational/test.cpp" "/home/anreydron/CLionProjects/520_B/algos/dron_a_a/build/prj.lab/rational/CMakeFiles/test_rational.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../prj.adds"
+  "../prj.lab"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/anreydron/CLionProjects/520_B/algos/dron_a_a/build/prj.lab/rational/CMakeFiles/rational.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
